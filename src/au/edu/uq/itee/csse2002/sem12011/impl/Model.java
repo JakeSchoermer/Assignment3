1066e822f1de51635fa3c44a17fa24c92246a465
Jake Schoermer <jake@ubuntu-server01.(none)>
2011-05-27 21:44:29 +1000

    Setup on Linux Comp

--- src/au/edu/uq/itee/csse2002/sem12011/impl/Model.java
+++ src/au/edu/uq/itee/csse2002/sem12011/impl/Model.java
@@ -1,154 +1,35 @@
 package au.edu.uq.itee.csse2002.sem12011.impl;
 
-import java.util.ArrayList;
-
-import au.edu.uq.itee.csse2002.sem12011.*;
 import au.edu.uq.itee.csse2002.sem12011.impl.*;
 
-/**
- * 
- * 
- * Playing the Game
- * 	play card from hand
- * 		select card to play and select subject card to play on
- * 		select "start round"
- * 		if no card selected, player picks up card from deck (or skips turn)
- * 	pick up card from deck
- * 	place subject card
- * 	freeze/unfreeze subject card
- * 	grade unfrozen subject card
- * 
- * Rules
- * 
- * 
- * Model uses ActionListeners 
- * 	lots of If -> Then statements to check what information
- * 		 needs to be sent to viewer
- * 
- * Start Game
- * 	load deck from specified file
- * 	shuffle deck
- * 	deal cards from top of deck
- * 		user gets first card
- * 		deal to each player in turn
- * 
- * */
-
 public class Model{
-	ArrayList<AbstractPlayer> players;
-	
-	
-	//shuffle deck
-	public void shuffleDeck() {
-		players.get(0).deck.shuffle();
-	}
-	
 	/**
-	 * Creates an ArrayList of players. This includes the user's player and
-	 * three opponents.
-	 * If the ArrayList already contains players, throw a StudentsLifeException.
-	 * @return an ArrayList<AbstractPlayers> that contains the players
-	 * @throws StudentsLifeException 
+	 * 
+	 * 
+	 * Playing the Game
+	 * 	play card from hand
+	 * 		select card to play and select subject card to play on
+	 * 		select "start round"
+	 * 		if no card selected, player picks up card from deck (or skips turn)
+	 * 	pick up card from deck
+	 * 	place subject card
+	 * 	freeze/unfreeze subject card
+	 * 	grade unfrozen subject card
+	 * 
+	 * Rules
+	 * 	if can't put down card, pick up card from deck
+	 * 	if no cards in deck, skip turn
+	 * 	if makes invalid selection, error message is displayed
+	 * 		new selection must be made
+	 * 
+	 * Model uses ActionListeners 
+	 * 	lots of If -> Then statements to check what information
+	 * 		 needs to be sent to viewer
+	 * 
 	 */
-	public void createPlayers(Deck deck) throws StudentsLifeException{
-		GUIPlayer user = new GUIPlayer(deck);
-		VirtualPlayer opponent1 = new VirtualPlayer(deck);
-		VirtualPlayer opponent2 = new VirtualPlayer(deck);
-		VirtualPlayer opponent3 = new VirtualPlayer(deck);
-		
-		players.add(user);
-		players.add(opponent1);
-		players.add(opponent2);
-		players.add(opponent3);
-	}
 	
 	
-	//deal first hand
-	public void dealFirstHand() throws BadDeckOperationException{
-		boolean keepDealing = true;
-		while (keepDealing) {
-			for (AbstractPlayer player: this.players){
-				if (player.deck.size() == 0)
-					throw new BadDeckOperationException("no more cards in deck");
-				
-				player.pickUp();
-				if (player.hand.size() == 5)
-					keepDealing = false;
-			}
-		}	
-	}
-	
-	public void playerMove(int inHandIndex, int inPlayIndex) {
-		Card playerCard = players.get(0).hand.get(inHandIndex);
-		if (playerCard instanceof SubjectCard) {
-			players.get(0).inPlay.add((SubjectCard) playerCard); 
-		}
-		
-		int cardOwner = this.getCoordinates(inPlayIndex).get(0);
-		int cardIndex = this.getCoordinates(inPlayIndex).get(1);
-		SubjectCard inPlayCard = players.get(cardOwner).inPlay.get(cardIndex);
-		
-		if (playerCard instanceof FreezeSubjectCard) {
-			FreezeSubjectCard freezeSubjectCard = 
-				(FreezeSubjectCardImpl) playerCard;
-			
-			if (freezeSubjectCard.getType().equals(
-					FreezeSubjectCard.FreezeSubjectCardType.NORMAL)) {
-				players.get(cardOwner).inPlay.get(cardIndex).
-				freezeWith((FreezeSubjectCard) playerCard);
-			}
-			
-			if (freezeSubjectCard.getType().equals(
-					FreezeSubjectCard.FreezeSubjectCardType.GRADE)) {
-				players.get(cardOwner).inPlay.get(cardIndex).
-				gradeWith((FreezeSubjectCard) playerCard);
-			}
-		}
-		players.get(0).hand.remove(inHandIndex);
-	}
-	
-	public ArrayList<Integer> getCoordinates(int inPlayIndex){
-		// TODO COMMENT
-		ArrayList<Integer> coordinates = new ArrayList<Integer>(2);
-		// Gets the owner and the position of the card being targeted (position meaning it's position within
-		// the ArrayList which holds all the inPlay )
-		int cardOwner = -1;
-		int cardIndex = -1;
-		int iterateCount = 0;
-		for (int player = 0; player < players.size(); player++) {
-			for (int position = 0; position < players.get(player).inPlay.size();
-			position++) {
-				iterateCount++;
-				if (iterateCount == inPlayIndex) {
-					cardOwner = player;
-					cardIndex = position;
-				}
-			}
-		} 
-		coordinates.add(cardOwner);
-		coordinates.add(cardIndex);
-		return coordinates;
-	}
 	
 	
-//	// puts a subject card in play
-//	// removes from hand
-//	public void playSubjectCard(int inHandIndex){
-//		SubjectCard card = (SubjectCard) players.get(0).hand.get(inHandIndex);
-//		players.get(0).inPlay.add(card);
-//	}
-	
-	
-//	// a subject card in play will be frozen
-//	// a freeze_subject_card will be removed from a players hand 
-//	public void playFreezeSubjectCard(int inHandIndex, int inPlayIndex){
-//	int cardOwner = this.getCoordinates(inPlayIndex).get(0);
-//	int cardIndex = this.getCoordinates(inPlayIndex).get(1);
-//	
-//	
-//	
-//	
-//	
-//	}
 	
 }
--- src/au/edu/uq/itee/csse2002/sem12011/impl/View.java
+++ src/au/edu/uq/itee/csse2002/sem12011/impl/View.java
@@ -19,108 +19,112 @@ import java.awt.event.*;
 public class View extends JFrame{
 	private static final int WIDTH = 1024;
 	private static final int HEIGHT = 600;
-
+	
 	private JLabel welcomeL, newPlayerL, numberOfOpponentsL, testL;
 	private JLabel Opponent1NameL, Opponent2NameL, Opponent3NameL, HumanNameL;
-	private JList Opp1CardsinPlayList;
+	private JLabel Opponent1CardsinPlayL;
+	private JList Opp1CardsinPlayList, HumanCardsInHandList;;
 	private JTextField newPlayerTF;
 	private JButton startGameBtn, exitBtn;
 	private JRadioButton oneOpponentRB;
 	private JRadioButton twoOpponentsRB;
 	private JRadioButton threeOpponentsRB;
-
+	
 	private StartGameButtonHandler sgbHandler;
 	private ExitButtonHandler ebHandler;
-
+	
 	private Border EmptyBorder = BorderFactory.createEmptyBorder(5,5,5,5);
-
+	
 	public View() {	
-
+		
 		Container titleGrid = new JPanel();
 		titleGrid.setBackground(Color.blue);
 		titleGrid.setSize(WIDTH, 50);
-
+		
 		welcomeL = new JLabel("Welcome to 'A Student's Life'", SwingConstants.CENTER);
 		welcomeL.setFont(new Font("Harrington" , Font.PLAIN, 24));
 		welcomeL.setForeground(Color.white);
 		welcomeL.setOpaque(false);
-
+		
 		titleGrid.add(welcomeL);
-
-
+		
+		
 		Container PlayerGrid = new JPanel();
 		PlayerGrid.setLayout(new GridLayout(2,2));
 		PlayerGrid.setBackground(Color.RED);
 		PlayerGrid.setSize(WIDTH, HEIGHT-150);
-
-				//Opponent1
+		
+			//Opponent1
 				Container Opponent1Grid = new JPanel();
-				Opponent1Grid.setLayout(new GridLayout(2,1));
+				Opponent1Grid.setLayout(new FlowLayout());
 					Opponent1NameL = new JLabel("Opponent 1", SwingConstants.LEFT);
+					Opponent1CardsinPlayL = new JLabel("Cards in Play", SwingConstants.LEFT);
 					String[] CardNames = {"Card 1, Card 2, Card 3"};	
 					Opp1CardsinPlayList = new JList(CardNames);
-					System.out.println(Opp1CardsinPlayList.getLayout());
-					//Opp1CardsinPlayList.setLayout(mgr)
+					Opp1CardsinPlayList.setBorder(BorderFactory.createLineBorder(Color.black));
+					Opp1CardsinPlayList.setSize(100, 100);
+					
 
+								
 				//Opponent2
 				Opponent2NameL = new JLabel("Opponent 2", SwingConstants.LEFT);
-
+				
 				//Opponent3
 				Opponent3NameL = new JLabel("Opponent 3", SwingConstants.LEFT);
-
+				
 				//Human
-				HumanNameL = new JLabel("Human", SwingConstants.LEFT);
-
-
-
+				Container HumanGrid = new JPanel();
+					HumanNameL = new JLabel("Human", SwingConstants.LEFT);
+					String[] HumanCardNames = {"Card 1, Card 2, Card 3"};	
+					HumanCardsInHandList = new JList(HumanCardNames);
+				
+						
 				PlayerGrid.add(Opponent1Grid);
-				Opponent1Grid.add(Opponent1NameL);
+				//Opponent1Grid.add(Opponent1NameL);
+				//Opponent1Grid.add(Opponent1CardsinPlayL);
 				Opponent1Grid.add(Opp1CardsinPlayList);
-
+				
 				PlayerGrid.add(Opponent2NameL);
 				PlayerGrid.add(Opponent3NameL);
-				PlayerGrid.add(HumanNameL);
+				
+				PlayerGrid.add(HumanGrid);
+				HumanGrid.add(HumanNameL);
+				HumanGrid.add(HumanCardsInHandList);
 
 		//Specify handlers and adds ActionListeners to start and exit buttons.
 		startGameBtn = new JButton("Start Game");
-
-
+		
+		
 		sgbHandler = new StartGameButtonHandler();  
 		startGameBtn.addActionListener(sgbHandler);
-
+		
 		exitBtn = new JButton("Exit Game");
 		ebHandler = new ExitButtonHandler();
 		exitBtn.addActionListener(ebHandler);
-
-
+		
+				
 		//ControlPanel		
 		Container ControlPanelGrid = new JPanel();
 		ControlPanelGrid.setLayout(new FlowLayout());
 		ControlPanelGrid.setBackground(Color.GREEN);
 		ControlPanelGrid.setSize(WIDTH, 100);
-
-
+		
+		
 		ControlPanelGrid.add(startGameBtn, BorderLayout.WEST);
 		ControlPanelGrid.add(exitBtn);
 
-
+		
 		newPlayerL = new JLabel("Enter your name: ", SwingConstants.RIGHT);
 		newPlayerTF = new JTextField(10);
 		numberOfOpponentsL = new JLabel("Choose your number of opponents: ", 
 				SwingConstants.CENTER);
-
-
-
-
-
-
-
-
+		
+		
 		setTitle("A Students Life");
-
+		
 		Container pane = this.getContentPane();
-
-
+		
+		
 		//Instantiate GUI Elements
 		pane.add (titleGrid, BorderLayout.NORTH);	
 		pane.add(PlayerGrid, BorderLayout.CENTER);
@@ -129,14 +133,13 @@ public class View extends JFrame{
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		//setResizable(false);
-
-
+		
+		
 	}
-
+	
 	private class StartGameButtonHandler implements ActionListener{
 	public void actionPerformed(ActionEvent e){
-		
-		//TODO
+	//TODO
 		//stores name of player
 //		if(newPlayerTF != null){
 //			AbstractPlayer.addOpponent().name(newPlayerTF);
@@ -153,7 +156,7 @@ public class View extends JFrame{
 //			AbstractPlayer.addOpponent().name(opponent3);
 //		}	
 //			
-
+			
 		System.load("game");
 	}
 	}
